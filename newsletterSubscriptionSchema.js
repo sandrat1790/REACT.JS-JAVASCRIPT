714f7599cd1c460f3f1e8d1284410b069004d632
Sandra Truong <105508619+sandrat1790@users.noreply.github.com>
2022-10-06 14:51:43 -0600

    Delete newsletterSubscriptionSchema.js

--- newsletterSubscriptionSchema.js
+++
@@ -1,7 +0,0 @@
-import * as Yup from 'yup';
-
-const newsletterSubscriptionSchema = Yup.object().shape({
-    email: Yup.string().email('Invalid email').required('Required'),
-});
-
-export default newsletterSubscriptionSchema;
